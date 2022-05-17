e56e9aae26fd0b08a9cd6759c53e4c21ec259baf
Daniel Ring <DanielKRing1@gmail.com>
2022-05-16 18:06:47 -0700

    %npm_config_m%

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as RealmStack from './stackMethods';
 import StackRealmCache from './stackRealmCache';
+import { COLUMN_NAME_SNAPSHOT_TIMESTAMP } from './stackMethods/constants';
 
 export default {
     ...RealmStack,
     realmCache: StackRealmCache,
+    TIMESTAMP_COLUMN_KEY: COLUMN_NAME_SNAPSHOT_TIMESTAMP,
 };
